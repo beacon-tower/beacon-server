fb433b434f2643e7eb2a796200e526a67a028990
luckyhua-mac <zgh11051121@163.com>
2018-01-03 11:23:44 +0800

    提取swagger配置

--- api/src/main/java/com/beacon/global/config/SwaggerConfig.java
+++ api/src/main/java/com/beacon/global/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.beacon.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -24,13 +25,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Profile({"dev", "test"})
 public class SwaggerConfig {
 
+    @Value("${springfox.documentation.swagger.title}")
+    private String title;//API文档标题
+
+    @Value("${springfox.documentation.swagger.description}")
+    private String description;//API说明
+
+    @Value("${springfox.documentation.swagger.version}")
+    private String version;//API版本
+
+    @Value("${springfox.documentation.swagger.basePackage}")
+    private String basePackage;//API基础包名
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.beacon"))
+                .apis(RequestHandlerSelectors.basePackage(basePackage))
                 .paths(PathSelectors.any())
                 .build()
                 .ignoredParameterTypes(ApiIgnore.class);
@@ -38,10 +51,9 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Spring Boot中使用Swagger2构建RESTFUL API")
-                .description("api文档")
-                .termsOfServiceUrl("http://api.beacon.com")
-                .version("v1")
+                .title(title)
+                .description(description)
+                .version(version)
                 .build();
     }
 
